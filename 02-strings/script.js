adc0621b5ccd4be0aeafe6d6a4f741301c399674
marcopriests <prtmrc98@gmail.com>
2025-03-14 16:43:15 +0100

    [UPD] 02 - 2 fatto

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -8,6 +8,10 @@ console.log(mioNome);
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+let number = 14;
+let text = "Pecorino romano DOP";
+let bool = true;
+console.log(number, text, bool);
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
